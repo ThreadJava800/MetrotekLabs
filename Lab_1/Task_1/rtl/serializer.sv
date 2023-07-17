46fd2f451a90f28a4a2d1ef9ee2881e620ecb951
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-07-17 16:54:18 +0300

    Changed indents from tabs -> 2 spaces

--- Lab_1/Task_1/rtl/serializer.sv
+++ Lab_1/Task_1/rtl/serializer.sv
@@ -1,66 +1,66 @@
 module serializer #(
-    parameter MAX_WORD_LEN = 4'd15,
-    parameter TRUE         = 1'b1,
-    parameter FALSE        = 1'b0
+  parameter MAX_WORD_LEN = 4'd15,
+  parameter TRUE         = 1'b1,
+  parameter FALSE        = 1'b0
 )(
-    input  logic         clk_i,
-    input  logic         srst_i,
-    input  logic [15:0]  data_i,
-    input  logic [3:0 ]  data_mod_i,
-    input  logic         data_val_i,
+  input  logic         clk_i,
+  input  logic         srst_i,
+  input  logic [15:0]  data_i,
+  input  logic [3:0 ]  data_mod_i,
+  input  logic         data_val_i,
 
-    output logic         ser_data_o,
-    output logic         ser_data_val_o,
-    output logic         busy_o 
+  output logic         ser_data_o,
+  output logic         ser_data_val_o,
+  output logic         busy_o 
 );
 
 logic [3:0] num_cnt;
 logic       valid_mod;
 
 always_ff @( posedge clk_i )
-    begin
-        // синхронный сброс
-        if ( srst_i )
+  begin
+      // синхронный сброс
+      if ( srst_i )
+        begin
+          ser_data_o     <= 1'b0;
+          busy_o         <= FALSE;
+          ser_data_val_o <= TRUE;
+
+          valid_mod      <= TRUE;
+
+          if ( data_mod_i == 0 )
+            num_cnt <= MAX_WORD_LEN;
+          else
             begin
-                ser_data_o     <= 1'b0;
-                busy_o         <= FALSE;
-                ser_data_val_o <= TRUE;
+              if ( ( data_mod_i == 1 ) || ( data_mod_i == 2 ) )
+                valid_mod <= FALSE;
+              else
+                valid_mod <= TRUE;
 
-                valid_mod      <= TRUE;
-
-                if ( data_mod_i == 0 )
-                    num_cnt <= MAX_WORD_LEN;
-                else
-                    begin
-                        if ( ( data_mod_i == 1 ) || ( data_mod_i == 2 ) )
-                            valid_mod <= FALSE;
-                        else
-                            valid_mod <= TRUE;
-
-                        num_cnt <= data_mod_i;
-                    end
+                num_cnt <= data_mod_i;
             end
-        else
+        end
+      else
+        begin
+          if ( ( data_val_i ) && ( valid_mod ) )
             begin
-                if ( ( data_val_i ) && ( valid_mod ) )
-                    begin
-                        if ( num_cnt != 0 )
-                            begin
-                                ser_data_o     <= data_i[MAX_WORD_LEN - num_cnt];
-                                busy_o         <= TRUE;
-                                ser_data_val_o <= FALSE;
+              if ( num_cnt != 0 )
+                begin
+                  ser_data_o     <= data_i[MAX_WORD_LEN - num_cnt];
+                  busy_o         <= TRUE;
+                  ser_data_val_o <= FALSE;
 
-                                num_cnt <= num_cnt - 1'b1;
-                            end
-                        else
-                            begin
-                                ser_data_o     <= data_i[MAX_WORD_LEN];
-                                busy_o         <= FALSE;
-                                ser_data_val_o <= TRUE;
-                            end
-                    end
+                  num_cnt <= num_cnt - 1'b1;
+                end
+              else
+                begin
+                  ser_data_o     <= data_i[MAX_WORD_LEN];
+                  busy_o         <= FALSE;
+                  ser_data_val_o <= TRUE;
+                end
             end
+        end
 
-    end
+  end
 
 endmodule
